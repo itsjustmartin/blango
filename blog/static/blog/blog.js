e8bda37cdc2e5a264b112380c4ba3f009dbd3ceb
Martin yacoub <abomilad7323@gmail.com>
2023-02-22 12:04:04 +0000

    Finish ReactJS

--- blog/static/blog/blog.js
+++ blog/static/blog/blog.js
@@ -30,3 +30,46 @@ p.then(resolvedCallback, rejectedCallback)
 // There will be some console output now
 
 lazyAdd("nan", "alsonan").then(resolvedCallback, rejectedCallback)
+
+
+// since you are including the blog.js file in your template after the React scripts,
+// you will have access to the React variable automatically.
+class ClickButton extends React.Component {
+  state = {
+    wasClicked: false
+  }
+
+  handleClick () {
+    this.setState(
+      {wasClicked: true}
+    )
+  }
+
+  render () {
+    let buttonText
+
+    if (this.state.wasClicked)
+      buttonText = 'Clicked!'
+    else
+      buttonText = 'Click Me'
+
+    return React.createElement(
+      'button',
+      {
+        className: 'btn btn-primary mt-2',
+        onClick: () => {
+          this.handleClick()
+        }
+      },
+      buttonText
+    )
+  }
+}
+
+
+const domContainer = document.getElementById('react_root')
+
+ReactDOM.render(
+  React.createElement(ClickButton),
+  domContainer
+)
