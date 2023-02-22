3f5e5e3812f22b8cb94ac562b6df138006dd5759
Martin yacoub <abomilad7323@gmail.com>
2023-02-22 13:21:52 +0000

    Finish JSX

--- blog/static/blog/blog.js
+++ blog/static/blog/blog.js
@@ -1,75 +1,87 @@
-// third party side or lib (Promise Construction)
-const lazyAdd = function (a, b) {
-  const doAdd = (resolve, reject) => {
-    if (typeof a !== "number" || typeof b !== "number") {
-      reject("a and b must both be numbers")
+// hardcoding for now without fetching from API in js
+class PostRow extends React.Component {
+  render () {
+    const post = this.props.post
+
+    let thumbnail
+
+    if (post.hero_image.thumbnail) {
+      thumbnail = <img src={post.hero_image.thumbnail}/>
     } else {
-      const sum = a + b
-      resolve(sum)
+      thumbnail = '-'
+    }
+
+    return <tr>
+      <td>{post.title}</td>
+      <td>
+        {thumbnail}
+      </td>
+      <td>{post.tags.join(', ')}</td>
+      <td>{post.slug}</td>
+      <td>{post.summary}</td>
+      <td><a href={'/post/' + post.slug + '/'}>View</a></td>
+    </tr>
+  }
+}
+
+class PostTable extends React.Component {
+  state = {
+    dataLoaded: true,
+    data: {
+      results: [
+        {
+          id: 15,
+          tags: [
+            'django', 'react'
+          ],
+          'hero_image': {
+            'thumbnail': '/media/__sized__/hero_images/snake-419043_1920-thumbnail-100x100-70.jpg',
+            'full_size': '/media/hero_images/snake-419043_1920.jpg'
+          },
+          title: 'Test Post',
+          slug: 'test-post',
+          summary: 'A test post, created for Django/React.'
+        }
+      ]
     }
   }
 
-  return new Promise(doAdd)
-}
-//---------------------
-// Executing side (Executing Promises)
-function resolvedCallback(data) {
-  console.log('Resolved with data ' +  data)
-}
-
-function rejectedCallback(message) {
-  console.log('Rejected with message ' + message)
-}
-
-const p = lazyAdd(3, 4)
-// p is a Promise instance that has not yet been settled
-// There will be no console output at this point.
-
-// This next line will settle the doAdd function
-p.then(resolvedCallback, rejectedCallback)
-// There will be some console output now
-
-lazyAdd("nan", "alsonan").then(resolvedCallback, rejectedCallback)
-
-
-// since you are including the blog.js file in your template after the React scripts,
-// you will have access to the React variable automatically.
-class ClickButton extends React.Component {
-  state = {
-    wasClicked: false
-  }
-
-  handleClick () {
-    this.setState(
-      {wasClicked: true}
-    )
-  }
-
   render () {
-    let buttonText
+    let rows
+    if (this.state.dataLoaded) {
+      if (this.state.data.results.length) {
+        rows = this.state.data.results.map(post => <PostRow post={post} key={post.id}/>)
+      } else {
+        rows = <tr>
+          <td colSpan="6">No results found.</td>
+        </tr>
+      }
+    } else {
+      rows = <tr>
+        <td colSpan="6">Loading&hellip;</td>
+      </tr>
+    }
 
-    if (this.state.wasClicked)
-      buttonText = 'Clicked!'
-    else
-      buttonText = 'Click Me'
-
-    return React.createElement(
-      'button',
-      {
-        className: 'btn btn-primary mt-2',
-        onClick: () => {
-          this.handleClick()
-        }
-      },
-      buttonText
-    )
+    return <table className="table table-striped table-bordered mt-2" >
+      <thead>
+      <tr style={{ color: 'white' }}>
+        <th>Title</th>
+        <th>Image</th>
+        <th>Tags</th>
+        <th>Slug</th>
+        <th>Summary</th>
+        <th>Link</th>
+      </tr>
+      </thead>
+      <tbody>
+      {rows}
+      </tbody>
+    </table>
   }
 }
-
 
 const domContainer = document.getElementById('react_root')
-
 ReactDOM.render(
-  React.createElement(ClickButton),
+  React.createElement(PostTable),
   domContainer
 )
