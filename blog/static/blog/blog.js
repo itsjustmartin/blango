f751aa0161fdbd1f0dec0149ed5962f9346fae79
Martin yacoub <abomilad7323@gmail.com>
2023-02-22 10:32:20 +0000

    Finish JavaScript functions

--- blog/static/blog/blog.js
+++ blog/static/blog/blog.js
@@ -1,1 +1,21 @@
-alert('Hello, world!')
+const doubler = x => x * 2 //lambda
+console.log(doubler(2))  // outputs 4
+
+function sayHello(yourName) {
+  if (yourName === undefined) {
+      console.log('Hello, no name')
+  } else {
+       console.log('Hello, ' + yourName)
+  }
+}
+
+const yourName = 'Your Name' 
+
+console.log('Before setTimeout')
+
+setTimeout(() => {
+    sayHello(yourName)
+  }, 2000
+)
+
+console.log('After setTimeout')
